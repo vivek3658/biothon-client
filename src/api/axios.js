fb2464e456b05fba9ad9ed6bf0f33b1369b55a0f
Vivek Makwana <vivekmakwana9408@gmail.com>
2026-07-21 23:43:45 +0530

    Url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const VERCEL_API_HOST = 'https://arogyax-server.vercel.app';
+const VERCEL_API_HOST = 'https://arogyax-server.onrender.com';
 const BASE_URL = import.meta.env.VITE_API_URL || VERCEL_API_HOST;
 
 export const apiClient = axios.create({
